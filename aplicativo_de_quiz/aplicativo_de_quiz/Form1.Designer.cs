3e15c26fc25b83d6188956ec97d1e725e4e84e7b
kookpa <141832427+kookpa@users.noreply.github.com>
2023-11-11 14:58:54 -0300

    Add files via upload

--- aplicativo_de_quiz/aplicativo_de_quiz/Form1.Designer.cs
+++ aplicativo_de_quiz/aplicativo_de_quiz/Form1.Designer.cs
@@ -28,13 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblPerguntas = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.txbResposta = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPerguntas
+            // 
+            this.lblPerguntas.AutoSize = true;
+            this.lblPerguntas.Location = new System.Drawing.Point(63, 59);
+            this.lblPerguntas.Name = "lblPerguntas";
+            this.lblPerguntas.Size = new System.Drawing.Size(35, 13);
+            this.lblPerguntas.TabIndex = 0;
+            this.lblPerguntas.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(64, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 56);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(64, 172);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(95, 51);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(183, 97);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(83, 55);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(183, 172);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(82, 50);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(297, 59);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(35, 13);
+            this.lblContador.TabIndex = 5;
+            this.lblContador.Text = "label2";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(306, 137);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(61, 42);
+            this.btnVerificar.TabIndex = 6;
+            this.btnVerificar.Text = "button1";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            // 
+            // txbResposta
+            // 
+            this.txbResposta.Location = new System.Drawing.Point(389, 82);
+            this.txbResposta.Name = "txbResposta";
+            this.txbResposta.Size = new System.Drawing.Size(78, 20);
+            this.txbResposta.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbResposta);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblPerguntas);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPerguntas;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lblContador;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txbResposta;
     }
 }
 
--- aplicativo_de_quiz/aplicativo_de_quiz/Form1.cs
+++ aplicativo_de_quiz/aplicativo_de_quiz/Form1.cs
@@ -12,9 +12,117 @@ namespace aplicativo_de_quiz
 {
     public partial class Form1 : Form
     {
+        // Criando uma classe para representar uma pergunta do quiz
+        public class Pergunta
+        {
+            public string Texto { get; set; } // O texto da pergunta
+            public string[] Alternativas { get; set; } // As quatro alternativas de resposta
+            public int Resposta { get; set; } // O índice da alternativa correta (0 a 3)
+
+            // Um construtor que recebe os parâmetros da pergunta
+            public Pergunta(string texto, string[] alternativas, int resposta)
+            {
+                Texto = texto;
+                Alternativas = alternativas;
+                Resposta = resposta;
+            }
+        }
+        // Criando uma lista de perguntas para o quiz
+        public List<Pergunta> perguntas = new List<Pergunta>()
+        {
+              new Pergunta("Qual é a capital do Brasil?", new string[] { "Rio de Janeiro", "São Paulo", "Brasília", "Salvador" }, 2),
+            new Pergunta("Qual é o maior país do mundo em área?", new string[] { "China", "Estados Unidos", "Rússia", "Canadá" }, 2),
+            new Pergunta("Qual é o nome do maior osso do corpo humano?", new string[] { "Fêmur", "Úmero", "Tíbia", "Fíbula" }, 0),
+            new Pergunta("Qual é o nome do autor de Dom Quixote?", new string[] { "Miguel de Cervantes", "William Shakespeare", "Jorge Luis Borges", "Gabriel García Márquez" }, 0),
+            new Pergunta("Qual é o nome do elemento químico de símbolo Au?", new string[] { "Argônio", "Alumínio", "Ouro", "Urânio" }, 2)
+        };
+        // Criando variáveis para armazenar o índice da pergunta atual, a pontuação e o número de perguntas
+        public int indice = 0;
+        public int pontuacao = 0;
+        public int total = 5;
+
+        // Criando um objeto da classe Random para gerar números aleatórios
+        public Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Embaralhando a lista de perguntas
+            perguntas = Embaralhar(perguntas);
+
+            // Exibindo a primeira pergunta
+            ExibirPergunta();
+        }
+        // Um método para embaralhar uma lista usando o algoritmo de Fisher-Yates
+        public List<T> Embaralhar<T>(List<T> lista)
+        {
+            for (int i = lista.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                T temp = lista[i];
+                lista[i] = lista[j];
+                lista[j] = temp;
+            }
+            return lista;
+        }
+        // Um método para exibir a pergunta e atualizar o contador
+        public void ExibirPergunta()
+        {
+            // Verificando se ainda há perguntas na lista
+            if (indice < total)
+            {
+                // Obtendo a pergunta atual
+                Pergunta p = perguntas[indice];
+
+                // Atualizando o texto da label e da textbox
+                lblPerguntas.Text = p.Texto;
+                txbResposta.Text = "";
+                // Atualizando o contador
+                lblContador.Text = $"Pergunta {indice + 1} de {total}";
+            }
+            else
+            {
+                // Encerrando o quiz e mostrando a pontuação
+                EncerrarQuiz();
+            }
+        }
+        // Um método para encerrar o quiz e mostrar a pontuação
+        public void EncerrarQuiz()
+        {
+            // Desabilitando os controles
+            lblPerguntas.Enabled = false;
+            txbResposta.Enabled = false;
+            btnVerificar.Enabled = false;
+
+            // Mostrando a pontuação
+            MessageBox.Show($"Você acertou {pontuacao} de {total} perguntas.", "Quiz Encerrado");
+        }
+        // Um evento para verificar a resposta quando o botão for clicado
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            // Obtendo a resposta digitada pelo usuário
+            string resposta = txbResposta.Text.Trim().ToLower();
+
+            // Verificando se a resposta está correta
+            if (resposta(perguntas[indice].Resposta.ToLower()))
+            {
+                // Incrementando a pontuação
+                pontuacao++;
+
+                // Mostrando uma mensagem de acerto
+                MessageBox.Show("Resposta correta!", "Parabéns");
+            }
+            else 
+            {
+                // Mostrando uma mensagem de erro
+                MessageBox.Show($"Resposta errada. A resposta correta é: {perguntas[indice].Resposta}", "Que pena");
+            }
+            // Avançando para a próxima pergunta
+            indice++;
+            ExibirPergunta();
         }
     }
 }
